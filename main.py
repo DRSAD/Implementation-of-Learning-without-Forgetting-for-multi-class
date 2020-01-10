0be54cd96b5bf950d7811edd511d4bbffb008b22
zhuoyunli <30347144+zhuoyunli@users.noreply.github.com>
2020-01-10 20:19:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from LwF import LwFmodel
 from ResNet import resnet18_cbam
 parser=10
 numclass=int(100/parser)
+task_size=int(100/parser)
 feature_extractor=resnet18_cbam()
 img_size=32
 batch_size=128
@@ -10,7 +11,7 @@ memory_size=2000
 epochs=100
 learning_rate=2.0
 
-model=LwFmodel(numclass,feature_extractor,batch_size,epochs,learning_rate)
+model=LwFmodel(numclass,feature_extractor,batch_size,epochs,learning_rate,task_size)
 
 for i in range(10):
     model.beforeTrain()
